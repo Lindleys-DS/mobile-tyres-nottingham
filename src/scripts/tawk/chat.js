a01a4df791bac1ce12659b6ab6e766cba14ce7e7
Dean Spicer <dean.spicer@lindleys.co.uk>
2023-05-05 11:41:47 +0100

    Initial commit

---
+++ src/scripts/tawk/chat.js
@@ -0,0 +1,9 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/64548e9dad80445890eb3930/1gvl4q6o9';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+})();
